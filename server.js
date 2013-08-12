84ed5c4acd849ab5e0ec9ee1e40e78db1954f138
chuguixin <chuguixin@163.com>
2013-08-12 18:21:49 +0800

    add operating about db on for bae

--- server.js
+++ server.js
@@ -5,19 +5,33 @@
  subTitle = ''
  password = '356256384.sx';
  author = 'chuguixin';
- icon = 'file_name.ico';
+ icon = 'favicon.ico';
 
 /**
  * Module dependencies.
  */
 var siteUrl, imageUrl;
 var express = require('express')
+  , MemcacheStore = require('connect-memcache')(express)
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
 var app = express();
+
+//use mongoos operating mongodb
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema
+    ,ObjectId = Schema.ObjectId;
+var db_name = 'WZUaEmHEHLIsuOxrzFlI';                  // 数据库名，从云平台获取
+var db_host =  process.env.BAE_ENV_ADDR_MONGO_IP;      // 数据库地址
+var db_port =  +process.env.BAE_ENV_ADDR_MONGO_PORT;   // 数据库端口
+var username = process.env.BAE_ENV_AK;                 // 用户名
+var password = process.env.BAE_ENV_SK;                 // 密码
+
+var db = mongoose.connect(db_host + db_port + db_name).connection;
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
