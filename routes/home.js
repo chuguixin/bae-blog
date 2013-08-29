0956d141273fcc18474ce49d13034082257de647
chuguixin <chuguixin@163.com>
2013-08-29 10:05:55 +0800

    just edit home.js

--- routes/home.js
+++ routes/home.js
@@ -1,2 +1,20 @@
 //index router
-var blogConfig = require('../blogConfig');
+var blogConfig = require('../blogConfig'),
+    mongoose = require('mongoose'),
+    db       = require('../db'),
+    post = db.post,
+    title = blogConfig.title,
+    subTitle = blogConfig.subTitle;
+
+//index
+exports.index = function(req, res){ 
+
+    post.find({}).sort('-_id').execFind(function(err, posts){
+        if(posts){
+            res.render('home', { title: title, subTitle:subTitle, posts:posts, admin:req.session.admin});
+        }else{
+            res.render('home', { title: title, subTitle:subTitle, posts:null, admin:req.session.admin })
+        }
+    });
+
+};
